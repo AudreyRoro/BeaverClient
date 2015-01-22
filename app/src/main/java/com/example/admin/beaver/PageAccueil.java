90e5fc87f42f00bc199fbb9276ac67d1c9b82e86
Audrey ROLLAND <rolland.aud@gmail.com>
2015-01-22 14:14:01 +0100

    Ajout de la ListView

--- app/src/main/java/com/example/admin/beaver/PageAccueil.java
+++ app/src/main/java/com/example/admin/beaver/PageAccueil.java
@@ -1,25 +1,41 @@
 package com.example.admin.beaver;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.content.Intent;
 
+import com.example.admin.model.Event;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class PageAccueil extends ActionBarActivity {
+public class PageAccueil extends Activity {
 
     ListView listEvent;
     Button btnajoutevent;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +43,23 @@ public class PageAccueil extends ActionBarActivity {
         setContentView(R.layout.activity_page_accueil);
 
         btnajoutevent = (Button) findViewById(R.id.btnajoutevent);
-        listEvent = (ListView) findViewById(R.id.listEvent);
+        listEvent = (ListView) findViewById(R.id.list);
 
-        List<String> liste = new ArrayList<String>();
-        liste.add("Soirée du nouvel an");
-        liste.add("Crémaillère");
+        ArrayList<Event> liste = new ArrayList<Event>();
 
-        ArrayAdapter<String> adaptateur = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, liste);
+        EventAdapter adaptateur = new EventAdapter(this,liste);
+
         listEvent.setAdapter(adaptateur);
+
+
+
+
+
+
+
+
+
+
 
         btnajoutevent.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,7 +68,47 @@ public class PageAccueil extends ActionBarActivity {
                 startActivity(intent);
             }
         });
+
+
+            }
+
+    private List<Event> mListE;
+    private Context mContext;
+    private LayoutInflater mInflater;
+
+    public EventAdapter(Context context, List<Event> liste){
+
     }
+
+
+
+    public static String getEvent(String url, Event event) {
+        StringBuilder builder = new StringBuilder();
+        HttpClient client = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(url);
+        try {
+            HttpResponse response = client.execute(httpGet);
+            StatusLine statusLine = response.getStatusLine();
+            int statusCode = statusLine.getStatusCode();
+            if (statusCode == 200) {
+                HttpEntity entity = response.getEntity();
+                InputStream content = entity.getContent();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line);
+                }
+            } else {
+                Log.e(ParseJSON.class.toString(), "Failed to download file");
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return builder.toString();
+    }
+
 
 
 
@@ -70,3 +135,5 @@ public class PageAccueil extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
+
