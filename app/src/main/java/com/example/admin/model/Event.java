874ce1fefd19f121c1620d2dd85c7e8636d60f27
marianneIsep <Rw!lE8n!>
2015-01-22 17:04:28 +0100

    Création de la classe server Connection pour gérer intéractions client/serveur
    Modification de la page Création evenement pour récupérer les éléments de la page

--- app/src/main/java/com/example/admin/model/Event.java
+++ app/src/main/java/com/example/admin/model/Event.java
@@ -70,15 +70,4 @@ public class Event {
     public void setParticipants(Set<Participant> participants) {
         this.participants = participants;
     }
-
-    public Event (int id, String title){
-        id = this.eId;
-        title=this.eTitle;
-    }
-
-    @Override
-    public String toString(){
-        return this.eTitle;
-    }
-
 }
